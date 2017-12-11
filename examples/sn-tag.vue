df36e6cb2b2937dfbbc667d24662628909d54810
SuperNever <1021165525@qq.com>
2017-12-11 22:55:49 +0800

    init

---
+++ examples/sn-tag.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Demo closable :clickEvt="clickFun">tag1</Demo>
+    <Demo :closable="false">tag2</Demo>
+    <Demo :closable="false">这是一个tag按钮</Demo>
+  </div>
+</template>
+
+<script>
+  import Demo from '../src/sn-tag/sn-tag';
+  export default {
+    name: 'app',
+    components: {
+      Demo
+    },
+    methods: {
+      clickFun() {
+        alert('delete');
+      }
+    }
+  };
+</script>
+<style lang="less" >
+  @import url(../static/styles/sn-tag.less);
+</style>
---
+++ src/sn-tag/sn-tag.vue
@@ -0,0 +1,38 @@
+<template>
+    <transition :name="openTransition ? '' : 'sn-fade-enter'">
+        <span
+                class="sn-tag"
+        >
+            <slot></slot>
+            <i class="snfont sn-close"
+               v-if="closable" @click="closeCallBack"
+            ></i>
+        </span>
+    </transition>
+</template>
+
+<script>
+    /* eslint-disable indent */
+    export default {
+        name: 'SNTag',
+        props: {
+            openTransition: {
+                type: Boolean,
+                default: true
+            },
+            closable: {
+                type: Boolean,
+                default: false
+            },
+            clickEvt: {
+                type: Function,
+                default: ()=>{}
+            }
+        },
+        methods: {
+            closeCallBack() {
+                this.clickEvt();
+            }
+        }
+    };
+</script>
